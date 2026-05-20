0b34cd2e6646fc2450b21416288a0f8dd86a1895
raphael <raphael.toubol@gmail.com>
2026-05-20 20:11:11 +0200

    fixing private routing to nat

--- awsTerraform/vpc/nat.tf
+++ awsTerraform/vpc/nat.tf
@@ -11,4 +11,6 @@ resource "aws_nat_gateway" "nat" {
   tags = {
     Name = "nat"
   }
+
+  depends_on = [aws_internet_gateway.igw]
 }
--- awsTerraform/vpc/priv-routes.tf
+++ awsTerraform/vpc/priv-routes.tf
@@ -1,23 +1,20 @@
-resource "aws_route_table" "private_zone1" {
+resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
   tags = {
     Name = "Private Route Table Zone 1"
   }
 }
 
-resource "aws_route_table" "private_zone2" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "Private Route Table Zone 2"
-  }
-}
-
 resource "aws_route_table_association" "private_zone1" {
   subnet_id      = aws_subnet.private_zone1.id
-  route_table_id = aws_route_table.private_zone1.id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "private_zone2" {
   subnet_id      = aws_subnet.private_zone2.id
-  route_table_id = aws_route_table.private_zone2.id
+  route_table_id = aws_route_table.private.id
 }
